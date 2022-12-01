6b50f931b2541ffa6b58e7db6003df2e469b9107
Sebastian <ss@digio.ch>
2022-12-01 15:06:09 +0100

    Fix getExisting Question time comparison

--- src/Repository/Quap/QuestionRepository.php
+++ src/Repository/Quap/QuestionRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository\Quap;
 use App\Entity\Quap\Question;
 use App\Entity\Quap\Questionnaire;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -45,12 +46,34 @@ class QuestionRepository extends ServiceEntityRepository
 
     public function getExisting(int $aspectId, string $dateTime)
     {
-        return $this->createQueryBuilder("q")
-            ->where("q.aspect = :aspectId")
-            ->andWhere('((q.deletedAt IS NULL OR q.deletedAt >= :date) AND q.createdAt <= :date)')
-            ->setParameter("aspectId", $aspectId)
-            ->setParameter("date", $dateTime)
-            ->getQuery()
-            ->getResult();
+        $rsm = new ResultSetMapping();
+        $rsm->addEntityResult(Question::class, 'q');
+        $rsm->addFieldResult('q', 'id', 'id');
+        $rsm->addMetaResult('q', 'aspect_id', 'aspect');
+        $rsm->addMetaResult('q', 'local_id', 'local_id');
+        $rsm->addFieldResult('q', 'question_de', 'question_de');
+        $rsm->addFieldResult('q', 'question_fr', 'question_fr');
+        $rsm->addFieldResult('q', 'question_it', 'question_it');
+        $rsm->addFieldResult('q', 'answer_options', 'answer_options');
+        $rsm->addFieldResult('q', 'created_at', 'createdAt');
+        $rsm->addFieldResult('q', 'deleted_at', 'deletedAt');
+        $rsm->addFieldResult('q', 'evaluation_function', 'evaluation_function');
+        $query = $this->_em->createNativeQuery(
+            "SELECT
+                    *
+                FROM
+                    hc_quap_question AS q
+                WHERE
+                    q.aspect_id = ?
+                    AND((q.deleted_at IS NULL
+                        OR q.deleted_at >= ?)
+                    AND q.created_at::date <= ?);",
+            $rsm
+        );
+        $query->setParameter(1, $aspectId);
+        $query->setParameter(2, $dateTime);
+        $query->setParameter(3, $dateTime);
+
+        return $query->getResult();
     }
 }
