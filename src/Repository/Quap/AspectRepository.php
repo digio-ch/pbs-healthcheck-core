d84305aa146f6d6e66144d1fabd291a7dd4a30b6
Sebastian <ss@digio.ch>
2022-12-01 14:57:44 +0100

    Fix getExisting Aspect time comparison

--- src/Repository/Quap/AspectRepository.php
+++ src/Repository/Quap/AspectRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository\Quap;
 
 use App\Entity\Quap\Aspect;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -21,12 +22,35 @@ class AspectRepository extends ServiceEntityRepository
 
     public function getExisting(int $questionnaireId, string $dateTime)
     {
-        return $this->createQueryBuilder("a")
-            ->where("a.questionnaire = :questionnaireId")
-            ->andWhere('((a.deletedAt IS NULL OR a.deletedAt >= :date) AND a.createdAt <= :date)')
-            ->setParameter("questionnaireId", $questionnaireId)
-            ->setParameter("date", $dateTime)
-            ->getQuery()
-            ->getResult();
+        $rsm = new ResultSetMapping();
+        $rsm->addEntityResult(Aspect::class, 'a');
+        $rsm->addFieldResult('a', 'id', 'id');
+        $rsm->addMetaResult('a', 'questionnaire_id', 'questionnaire');
+        $rsm->addMetaResult('a', 'local_id', 'local_id');
+        $rsm->addFieldResult('a', 'name_de', 'name_de');
+        $rsm->addFieldResult('a', 'name_fr', 'name_fr');
+        $rsm->addFieldResult('a', 'name_it', 'name_it');
+        $rsm->addFieldResult('a', 'created_at', 'createdAt');
+        $rsm->addFieldResult('a', 'deleted_at', 'deletedAt');
+        $rsm->addFieldResult('a', 'description_de', 'descriptionDe');
+        $rsm->addFieldResult('a', 'description_fr', 'descriptionFr');
+        $rsm->addFieldResult('a', 'description_it', 'descriptionIt');
+        $query = $this->_em->createNativeQuery(
+            "SELECT
+                    *
+                FROM
+                    hc_quap_aspect AS a
+                WHERE
+                    a.questionnaire_id = ?
+                    AND((a.deleted_at IS NULL
+                        OR a.deleted_at >= ?)
+                    AND a.created_at::date <= ?);",
+            $rsm
+        );
+        $query->setParameter(1, $questionnaireId);
+        $query->setParameter(2, $dateTime);
+        $query->setParameter(3, $dateTime);
+
+        return $query->getResult();
     }
 }
