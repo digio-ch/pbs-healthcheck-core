abf70d8b2ef1acea3a2be441fa055397a78744f7
Sebastian <ss@digio.ch>
2023-06-21 11:49:08 +0200

    add user information to sentry and clean up .env files

--- src/Service/Sentry.php
+++ src/Service/Sentry.php
@@ -2,10 +2,24 @@
 
 namespace App\Service;
 
+use App\DTO\Model\PbsUserDTO;
+use Sentry\UserDataBag;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Security;
 
 class Sentry
 {
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+    /**
+     * Filter out acceptable Excepitons (HTTP 401) and sensitive data.
+     * @return callable
+     */
     public function sentryFilter(): callable
     {
         return function (\Sentry\Event $event, ?\Sentry\EventHint $hint): ?\Sentry\Event {
@@ -14,6 +28,15 @@ class Sentry
                 return null;
             }
 
+            $user = $this->security->getUser();
+            if ($user instanceof PbsUserDTO) {
+                $userBag = new UserDataBag($user->getId(), null, null, null, null);
+                $userBag->setMetadata('Nickname', $user->getNickName());
+            } else {
+                $userBag = new UserDataBag(null, null, null, $user->getUsername(), null);
+                $userBag->setMetadata('Is PbsUserDTO', false);
+            }
+            $event->setUser($userBag);
             return $event;
         };
     }
