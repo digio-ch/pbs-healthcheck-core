c434c98e993ac69c351cb58b8930ab545bbea9f9
Sebastian <ss@digio.ch>
2024-02-08 15:43:08 +0100

    add comment

--- src/Service/Census/CensusDateProvider.php
+++ src/Service/Census/CensusDateProvider.php
@@ -13,6 +13,13 @@ class CensusDateProvider
         $this->censusGroupRepository = $censusGroupRepository;
     }
 
+    /**
+     * @return int
+     * @throws \Exception
+     * This funciton returns the latest year we have census data for in the database.
+     * It was created so that census widgets are still usable in the period between the start of a new year until
+     * the census data is updated. 
+     */
     public function getLatestYear(): int
     {
         return $this->censusGroupRepository->getLatestYear();
