4f76b58f0a5ae1282aa6e4187dcc2f5db3d94ae7
Léonard Roch <lr@digio.ch>
2025-02-19 15:43:28 +0100

    fixed linting errors

--- src/Repository/General/StatusMessageRepository.php
+++ src/Repository/General/StatusMessageRepository.php
@@ -11,7 +11,6 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class StatusMessageRepository extends ServiceEntityRepository
 {
-
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, StatusMessage::class);
