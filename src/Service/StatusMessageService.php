f57007fc879100a89a35e491d5ae125e6849eda6
Léonard Roch <lr@digio.ch>
2025-02-19 12:45:53 +0100

    removed JSON validation for status message from database

--- src/Service/StatusMessageService.php
+++ src/Service/StatusMessageService.php
@@ -6,10 +6,7 @@ use App\DTO\Mapper\StatusMessageMapper;
 use App\DTO\Model\StatusMessageDTO;
 use App\Entity\General\StatusMessage;
 use App\Exception\ApiException;
-use App\Model\LogMessage\SimpleLogMessage;
 use App\Repository\General\StatusMessageRepository;
-use Digio\Logging\GelfLogger;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class StatusMessageService
 {
@@ -19,25 +16,11 @@ class StatusMessageService
     private StatusMessageRepository $statusRepo;
 
     /**
-     * @var TranslatorInterface translator
-     */
-    private TranslatorInterface $translator;
-
-    /**
-     * @var GelfLogger
-     */
-    private GelfLogger $logger;
-
-    /**
      * @param StatusMessageRepository $statusRepo
-     * @param TranslatorInterface $translator
-     * @param GelfLogger $logger
      */
-    public function __construct(StatusMessageRepository $statusRepo, TranslatorInterface $translator, GelfLogger $logger)
+    public function __construct(StatusMessageRepository $statusRepo)
     {
         $this->statusRepo = $statusRepo;
-        $this->translator = $translator;
-        $this->logger = $logger;
     }
 
 
@@ -54,21 +37,7 @@ class StatusMessageService
             return new StatusMessageDTO(StatusMessage::NONE);
         }
 
-        $dto = StatusMessageMapper::mapStatusBanner($state, $lang);
-
-        $message = $dto->getMessage();
-
-        $jsonFields = json_decode($message, true);
-
-        if ($jsonFields === null || count($jsonFields) != 2 ||
-           !array_key_exists("title", $jsonFields) || !is_string($jsonFields["title"]) ||
-           !array_key_exists("body", $jsonFields) || !is_string($jsonFields["body"])
-        ) {
-           $this->logger->warning(new SimpleLogMessage("status banner message is invalid: $message"));
-           throw new ApiException(500, $this->translator->trans('api.error.unknown'));
-        }
-
-        return $dto;
+        return StatusMessageMapper::mapStatusBanner($state, $lang);
     }
 
 
