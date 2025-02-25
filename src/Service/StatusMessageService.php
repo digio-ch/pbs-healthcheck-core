59fb9c8b7d157aaabf83b0def1655819c010f296
Léonard Roch <lr@digio.ch>
2025-02-25 15:52:25 +0100

    refactored status banner mapping to keep the message dynamic

--- src/Service/StatusMessageService.php
+++ src/Service/StatusMessageService.php
@@ -6,10 +6,7 @@ use App\DTO\Mapper\StatusBannerMapper;
 use App\DTO\Model\StatusBannerDTO;
 use App\Entity\General\StatusMessage;
 use App\Exception\ApiException;
-use App\Model\LogMessage\SimpleLogMessage;
 use App\Repository\General\StatusMessageRepository;
-use Digio\Logging\GelfLogger;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class StatusMessageService
 {
@@ -19,31 +16,12 @@ class StatusMessageService
     private StatusMessageRepository $statusRepo;
 
     /**
-     * @var TranslatorInterface translator
-     */
-    private TranslatorInterface $translator;
-
-    /**
-     * @var GelfLogger
-     */
-    private GelfLogger $logger;
-
-    /**
      * @param StatusMessageRepository $statusRepo
-     * @param TranslatorInterface $translator
-     * @param GelfLogger $logger
      */
-    public function __construct(
-        StatusMessageRepository $statusRepo,
-        TranslatorInterface $translator,
-        GelfLogger $logger
-    ) {
+    public function __construct(StatusMessageRepository $statusRepo)
+    {
         $this->statusRepo = $statusRepo;
-        $this->translator = $translator;
-        $this->logger = $logger;
     }
-
-
 
     /**
      * @param string $lang
@@ -58,13 +36,6 @@ class StatusMessageService
             return new StatusBannerDTO(StatusMessage::NONE);
         }
 
-        try {
-            return StatusBannerMapper::map($state, $lang);
-        } catch (\Throwable $e) {
-            $message = $state->getMessage($lang);
-
-            $this->logger->warning(new SimpleLogMessage("status banner message is invalid: $message"));
-            throw new ApiException(500, $this->translator->trans('api.error.unknown'));
-        }
+        return StatusBannerMapper::map($state, $lang);
     }
 }
