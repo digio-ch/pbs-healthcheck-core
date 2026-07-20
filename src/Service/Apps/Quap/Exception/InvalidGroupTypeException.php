1626dfff7cf687ca857ad36b2b3a4139d327b552
Léonard Roch <179218027+lr-digio@users.noreply.github.com>
2026-07-20 15:19:39 +0200

    chore: added strict typing using https://getrector.com/rule-detail/safe-declare-strict-types-rector

--- src/Service/Apps/Quap/Exception/InvalidGroupTypeException.php
+++ src/Service/Apps/Quap/Exception/InvalidGroupTypeException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Service\Apps\Quap\Exception;
 
 class InvalidGroupTypeException extends \Exception
--- src/Service/Apps/Quap/Exception/NoDataException.php
+++ src/Service/Apps/Quap/Exception/NoDataException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Service\Apps\Quap\Exception;
 
 class NoDataException extends \Exception
